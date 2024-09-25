0817aed74c4c093d3ccb0b4aad65b9e030d4be76
Marshall Maguire <marshallmaguire31@gmail.com>
2024-09-25 13:14:10 -0400

    Added pet post!

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const AboutPage = () => {
   return (
-    <main>
       <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
-    </main>
   )
 }
 
-export const Head = () => <title>About Me</title>
+export const Head = () => <Seo title="Home Page" />
 
 export default AboutPage
